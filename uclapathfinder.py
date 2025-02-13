ed1794d649e6336f61320536dae64de899b9de78
kguo12 <kennyguo@ucla.edu>
2025-02-12 21:44:00 -0800

    Created uclapathfinder.py
    Put in `uclapathfinder.py` with a `optimal_walk_route` method
    
    To-do suggestions to build on are included:
    1) Use google maps API to get coordinates from just inputting a random string (landmark/location)
    2) Gather data from other sources (put into sql database possibly) and change the weights of the walking paths to include other things besides length (e.g. busyness/student traffic/peak hours) **** This one is likely the most relevant and distinguishing for a project
    3) Make the plot actually "interactive", allow user to click where to start and end

---
+++ uclapathfinder.py
@@ -0,0 +1,48 @@
+import networkx as nx
+import osmnx as ox
+import plotly.graph_objects as go
+import plotly.offline as pyo
+
+def optimal_walk_route(start, end):
+    '''
+    FURTHER TO DOS
+    1) start and end work right now as tuples of coordinates (lat, long), potentially in future
+    user could provide a name of a landmark --> google maps API --> get coordinates --> input here
+    2) Also, the interactive map could be actually "interactive", allow user to click on the map,
+    store inputs into coordinates, then plot out optimal_route
+    3) the weights right now are only the lengths, should include data on busyness/high traffic walking
+    routes to change up the weights used by Shortest path algo
+    '''
+    
+    # Make graph of UCLA walking routes
+    G = ox.graph.graph_from_point((34.07, -118.445), dist=750, network_type="walk")
+
+    # Find the nearest nodes in the graph
+    start_node = ox.distance.nearest_nodes(G, start[1], start[0])
+    end_node = ox.distance.nearest_nodes(G, end[1], end[0])
+
+    # Print starting and ending locations
+    print("Start at: Latitude: ", G.nodes[start_node]['y'], "Longitude: ", G.nodes[start_node]['x'])
+    print("End at: Latitude: ", G.nodes[end_node]['y'], "Longitude: ", G.nodes[end_node]['x'])
+    
+    # Compute the shortest path, get the coordinates
+    shortest_path = nx.shortest_path(G, source=start_node, target=end_node, weight="length")
+    path_lats = [G.nodes[node]['y'] for node in shortest_path]
+    path_longs = [G.nodes[node]['x'] for node in shortest_path]
+    
+    # Plot out an interactive map of the shortest route
+    path = go.Scattermapbox(
+        lat=path_lats,
+        lon=path_longs,
+        mode='lines',
+    )
+    layout = go.Layout(
+        mapbox=dict(
+            style='open-street-map',
+            center=dict(lat=path_lats[len(path_lats)//2], lon=path_longs[len(path_longs)//2]),
+            zoom=20
+        ),
+        margin=dict(l=0, r=0, t=0, b=0)
+    )
+    fig = go.Figure(data=path, layout=layout)
+    pyo.iplot(fig)
